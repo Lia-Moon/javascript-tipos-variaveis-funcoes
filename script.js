83622050814b7769ab040033d916729be238cc12
Júlia Soares Vieira <juliasoaresvieira@hotmail.com>
2023-03-27 17:35:57 -0300

    testes javascript

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ const hiragana = '\u3041';
 console.log(cifrao);
 console.log(tique);
 console.log(hiragana);
+
+const cidade = "belo horizonte";
+const input = "Belo HORIZONTE";
+
+console.log("\n");
+console.log(cidade === input);
+
+const inputMinusculo = input.toLowerCase();
+
+console.log("\n");
+
+console.log(cidade === inputMinusculo);
+
+console.log("\n");
+
+const senha = "Jujusvieira";
+
+console.log(senha.length);
+
+console.log("\n");
