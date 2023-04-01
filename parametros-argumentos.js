c885501b609738668d5a76c45ccfbf0003106178
Júlia Soares Vieira <juliasoaresvieira@hotmail.com>
2023-04-01 01:10:21 -0300

    exercícios de parâmetros e argumentos

---
+++ parametros-argumentos.js
@@ -0,0 +1,9 @@
+function somaIdades(idade1, idade2, idade3) {
+    return idade1 + idade2 + idade3;
+}
+
+let idade1 = 20;
+let idade2 = 55;
+let idade3 = 28;
+
+console.log(somaIdades(idade1, idade2, idade3));
