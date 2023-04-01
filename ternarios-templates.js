fd1e350d9a01af5660d4f7d4bed2cb86e4bb0063
Júlia Soares Vieira <juliasoaresvieira@hotmail.com>
2023-03-31 23:29:01 -0300

    exercícios de Ternários e Templates

--- ternarios-templates.js
+++ ternarios-templates.js
@@ -6,3 +6,4 @@ const bebidaMenor = "suco";
 // diz: "por favor, quero beber"
 
 console.log(`${nome} diz: "por favor, quero beber ${idade >= 18 ? bebidaMaior : bebidaMenor}"`);
+console.log(nome + ' diz: "por favor, quero beber ' + (idade >= 18 ? bebidaMaior : bebidaMenor) + '"');
