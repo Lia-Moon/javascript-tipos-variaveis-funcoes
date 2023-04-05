6ed27641f105ccf6596d14e00ddcf804add044ee
Júlia Soares Vieira <juliasoaresvieira@hotmail.com>
2023-04-05 19:36:42 -0300

    aulas sobre funções

--- parametros-argumentos.js
+++ parametros-argumentos.js
@@ -7,3 +7,6 @@ let idade2 = 55;
 let idade3 = 28;
 
 console.log(somaIdades(idade1, idade2, idade3));
+
+console.log(soma(1, 1));
+var soma = function(num1, num2) { return num1 + num2 } // necessário declarar antes de ser usada
